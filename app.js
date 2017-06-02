17b56a1524df77fe5b237074b18fb87aaa072ace
Hervé <herve.frackowiak@gmail.com>
2017-06-02 17:36:16 +0200

    Debug

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ function processMessage(event) {
                 sendMessage(senderId, {text: randomize(messages.reponses.magCP)});
                 completeAdress(magasin.cp, false);
               } else if (re = formattedMsg.match(/\w{2,35}/)) {
-                magasin.ville = re[0];
+                magasin.ville = re[0].toLowerCase();
                 sendMessage(senderId, {text: randomize(messages.reponses.magVille)});
                 completeAdress(magasin.ville, true);
               } else {
@@ -210,7 +210,7 @@ function completeAdress(data, ville){
           //Default no null
           cp = bodyObj.cities[0].code;
           for (var i = 0; i < bodyObj.cities.length; i++) {
-            if(bodyObj.cities[i].city == data)
+            if(bodyObj.cities[i].city == data.toLowerCase())
             cp = bodyObj.cities[i].code;
           }
           var body = {"country": "FRANCE", "locality": data,"postalCode": cp};
